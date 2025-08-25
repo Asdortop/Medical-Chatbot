b2b7b292b147f88f282b319d3ef61cc5d8a9e0d9
Asdortop <tejas.gvsb@gmail.com>
2025-08-25 10:31:20 +0530

    Folder structure and initialization done

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Gen_AI_Medibot",
+    version='0.0.0',
+    author="Tejas",
+    author_email="tejas.gvsb@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
